db825b461d90a71c1e9b1c54353c9bab75fe7751
李恺林 <likailin@likailindeair.lan>
2016-11-10 19:24:28 +0800

    Initial Commit

---
+++ WaterFall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WaterFall
+//
+//  Created by 李恺林 on 2016/11/10.
+//  Copyright © 2016年 李恺林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
